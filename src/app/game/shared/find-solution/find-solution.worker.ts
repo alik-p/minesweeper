71b417710384f55e1c215455e5da79b5f97bd859
alik-p <aliaksandr.papko@gmaol.com>
2020-03-01 18:46:30 +0300

    feat: add 'find solution' worker

---
+++ src/app/game/shared/find-solution/find-solution.worker.ts
@@ -0,0 +1,44 @@
+/// <reference lib="webworker" />
+
+
+import { Probabilities } from './probabilities';
+import { Field, Minefield } from '../minefield';
+
+{
+
+  addEventListener('message', ({data}) => {
+    const minefield = new Minefield(data);
+    const solution = findSolution(minefield);
+    postMessage(solution);
+  });
+
+
+  const findSolution = (minefield: Minefield): Field[] => {
+    const probabilities = Probabilities.calculate(minefield);
+    const thresholdSafe = 0.05;
+    const thresholdMined = 0.9;
+    const solvedFields: Set<Field> = new Set<Field>();
+
+    probabilities.forEach((group) => {
+      group.fields.forEach(field => {
+        if (field.probability >= thresholdMined) {
+          field.mine = true;
+          solvedFields.add(field);
+        }
+      });
+      const min = group.probabilityMin;
+      // minGeneral = Math.min(minGeneral, min);
+      // TODO mined (???)
+      if (min <= thresholdSafe) {
+        const random = group.randomFieldMin;
+        solvedFields.add(random);
+      }
+    });
+
+    const demineFields = Array.from(solvedFields);
+    return demineFields;
+
+  };
+
+
+}
