0a9299bc424ac3a9a8bc70b03767bba086f0cb1c
alik-p <aliaksandr.papko@gmaol.com>
2020-03-29 16:04:31 +0300

    init: minefield.component.spec

---
+++ src/app/game/components/minefield/minefield.component.spec.ts
@@ -0,0 +1,9 @@
+import { MinefieldComponent } from './minefield.component';
+
+describe('Game - MinefieldComponent - TODO', () => {
+  let component: MinefieldComponent;
+
+  beforeEach(() => component = new MinefieldComponent());
+
+
+});
