edbe092be25f323f6c9698341add228757f46575
alik-p <aliaksandr.papko@gmaol.com>
2020-04-18 17:53:02 +0300

    init: onDemine() spec test

--- src/app/game/components/minefield/minefield.component.spec.ts
+++ src/app/game/components/minefield/minefield.component.spec.ts
@@ -1,17 +1,39 @@
 import { MinefieldComponent } from './minefield.component';
 import { Minefield } from '../../shared/minefield';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { click } from '../../../../testing';
+import { IMinefieldAction } from '../../shared/types/minefield-action';
+import { By } from '@angular/platform-browser';
 
 describe('Game / MinefieldComponent', () => {
   let component: MinefieldComponent;
+  let componentDebugElement: DebugElement;
+  let componentNativeElement: HTMLElement;
+  let fixture: ComponentFixture<MinefieldComponent>;
+
+  const minefieldCell = (x: number, y: number): HTMLElement => {
+    return componentNativeElement.querySelector<HTMLElement>(`.minefield--cell[data-position="${x},${y}"]`);
+  };
 
   beforeEach(() => {
-    component = new MinefieldComponent();
+    TestBed.configureTestingModule({
+      declarations: [MinefieldComponent]
+    });
+    fixture = TestBed.createComponent(MinefieldComponent);
+    component = fixture.componentInstance;
+    componentDebugElement = fixture.debugElement;
+    componentNativeElement = componentDebugElement.nativeElement;
+
     const data: string[][] = [
       ['1', '*', '?', '0'],
       ['?', '?', '?', '0'],
       ['0', '0', '0', '0'],
     ];
     component.minefield = new Minefield(data);
+
+    fixture.detectChanges();  // trigger initial data binding
+
   });
 
 
@@ -35,6 +57,14 @@ describe('Game / MinefieldComponent', () => {
 
 
   it('#onDemine() - TODO', () => {
+    // TODO
+    let action: IMinefieldAction;
+    const clickableEl = componentNativeElement.querySelector<HTMLElement>('.minefield');
+    const targetEl = minefieldCell(1, 0);
+    clickableEl.appendChild(targetEl);
+    const testEl = componentDebugElement.query(By.css('.minefield'));
+    click(testEl, {target: targetEl});
+    component.action.subscribe(res => action = res);
     expect(true).toBeTrue();
   });
 
